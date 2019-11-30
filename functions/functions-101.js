b92d047fc629a61bde0d2d52d09a526d6073665d
Hashim Warren <hashimwarren@gmail.com>
2019-11-30 10:12:22 -0500

    complete lesson 21 - functions

---
+++ functions/functions-101.js
@@ -0,0 +1,24 @@
+// funtion - input, code, output
+
+let greetUser = function () {
+
+}
+
+// challenge area
+
+// convertFahrenheitToCelcius
+
+// call a few times
+
+// print converted values
+
+let convertFahrenheitToCelcius = function (fahrenheit) {
+    let celcius = (fahrenheit - 32) / 1.8
+    return celcius 
+
+}
+
+
+ tempOne = convertFahrenheitToCelcius(80) //?
+tempTwo = convertFahrenheitToCelcius(32) //?
+tempThree = convertFahrenheitToCelcius(100) //?
