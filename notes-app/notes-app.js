e71554d96b4edec0b0eaf4b56537124cd698ee4c
Hashim Warren <hashimwarren@gmail.com>
2020-04-20 21:09:08 -0400

    add Mead's notes app files

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,52 +1,25 @@
-let notes = [];
+const notes = getSavedNotes()
 
 const filters = {
-  searchText: ""
-};
-
-// Check for existing saved data
-const notesJSON = localStorage.getItem("notes");
-
-if (notesJSON !== null) {
-  notes = JSON.parse(notesJSON);
+    searchText: ''
 }
 
-const renderNotes = function(notes, filters) {
-  const filteredNotes = notes.filter(function(note) {
-    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
-  });
+renderNotes(notes, filters)
 
-  document.querySelector("#notes").innerHTML = "";
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    notes.push({
+        title: '',
+        body: ''
+    })
+    saveNotes(notes)
+    renderNotes(notes, filters)
+})
 
-  filteredNotes.forEach(function(note) {
-    const noteEl = document.createElement("p");
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
 
-    if (note.title.length > 0) {
-      noteEl.textContent = note.title;
-    } else {
-      noteEl.textContent = "Unnamed note";
-    }
-
-    document.querySelector("#notes").appendChild(noteEl);
-  });
-};
-
-renderNotes(notes, filters);
-
-document.querySelector("#create-note").addEventListener("click", function(e) {
-  notes.push({
-    title: "",
-    body: ""
-  });
-  localStorage.setItem("notes", JSON.stringify(notes));
-  renderNotes(notes, filters);
-});
-
-document.querySelector("#search-text").addEventListener("input", function(e) {
-  filters.searchText = e.target.value;
-  renderNotes(notes, filters);
-});
-
-document.querySelector("#filter-by").addEventListener("change", function(e) {
-  console.log(e.target.value);
-});
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})
