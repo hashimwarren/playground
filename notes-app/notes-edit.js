977d499e97a3db893afd6d6953daf0cb5102c05c
Hashim Warren <hashimwarren@gmail.com>
2020-05-15 11:46:59 -0400

    add TODO comments for challenge

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -12,3 +12,10 @@ if (note === undefined) {
 
 document.querySelector('#note-title').value = note.title
 document.querySelector('#note-body').value = note.body
+
+// TODO
+// 1. setup input event for title
+// 2. update note object and save notes list
+// 3. reeat steps 1-2 for body
+// 4. setup a remove button the removes notes and sends users back to home page
+
