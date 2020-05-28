050c8ca02b8d66755b5a5866e545e223212dc68b
Hashim Warren <hashimwarren@gmail.com>
2020-05-27 23:12:31 -0400

    Refactor to more arrow functions

--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -26,7 +26,7 @@ const saveNotes = (notes) => {
 }
 
 // Generate the DOM structure for a note
-const generateNoteDOM = function (note) {
+const generateNoteDOM = (note) => {
     const noteEl = document.createElement('div')
     const textEl = document.createElement('a')
     const button = document.createElement('button')
@@ -59,9 +59,9 @@ const generateNoteDOM = function (note) {
 }
 
 // sort your note
-const sortNotes = function (notes, sortBy) {
+const sortNotes = (notes, sortBy) => {
     if (sortBy === 'byEdited') {
-        return notes.sort(function (a, b) {
+        return notes.sort((a, b) => {
             if (a.updatedAt > b.updatedAt) {
                 return -1
             } else if (a.updatedAt < b.updatedAt) {
@@ -72,7 +72,7 @@ const sortNotes = function (notes, sortBy) {
 
         })
     } else if (sortBy === 'byCreated') {
-        return notes.sort(function (a, b) {
+        return notes.sort((a, b) => {
             if (a.createdAt < b.createdAt) {
                 return -1
             } else if (a.createdAt > b.createdAt) {
@@ -83,7 +83,7 @@ const sortNotes = function (notes, sortBy) {
 
         })
     } else if (sortBy === 'alphabetical') {
-        return notes.sort(function (a, b) {
+        return notes.sort((a, b) => {
             if (a.title.toLowerCase() < b.title.toLowerCase()) {
                 return -1
             } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
@@ -103,7 +103,7 @@ const sortNotes = function (notes, sortBy) {
 }
 
 // Render application notes
-const renderNotes = function (notes, filter) {
+const renderNotes = (notes, filter) => {
     notes = sortNotes(notes, filter.sortBy)
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
@@ -111,13 +111,10 @@ const renderNotes = function (notes, filter) {
 
     document.querySelector('#notes').innerHTML = ''
 
-    filteredNotes.forEach(function (note) {
+    filteredNotes.forEach((note) => {
         const noteEl = generateNoteDOM(note)
         document.querySelector('#notes').appendChild(noteEl)
     })
 }
 
-const generateLastEdited = function (timestamp) {
-    return `Last edited ⏰: ${moment(timestamp).fromNow()}`
-
-}
+const generateLastEdited = (timestamp) => `Last edited ⏰: ${moment(timestamp).fromNow()}`
