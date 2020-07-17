7d1aebd42828c997495ec8fb1952a2c9e0747676
Hashim Warren <hashimwarren@gmail.com>
2020-07-16 20:59:11 -0400

    almost finished

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -9,23 +9,28 @@ const Hangman = function (word, remainingGuesses, guess) {
 
     this.word = word.toLowerCase().split(''),
         this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c']
 
 }
 
-// Hangman.prototype.getPuzzle = function () {
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
 
-//     //     if (this.guess = []) {
-//     //         return `***`
+    this.word.forEach(() => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
 
-//     //     } else {
-//     //         this.guess
-//     //         return `aaa`
+        } else {
+            puzzle += '*'
 
-//     //     }
+        }
+    })
 
 
+    return puzzle
 
-// }
+
+}
 
 const gameOne = new Hangman('Bathroom', 3, 'a')
 console.log(gameOne.guess);
