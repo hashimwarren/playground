306bdbd58e88ac9978103f8bf689b7193a971c70
Hashim Warren <hashimwarren@gmail.com>
2020-07-26 20:50:33 -0400

    finish lesson 105

--- hangman/app.js
+++ hangman/app.js
@@ -8,7 +8,7 @@ const game1 = new Hangman('Car Parts', 2)
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.puzzle
@@ -19,11 +19,13 @@ window.addEventListener('keypress', function (e) {
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
         console.log(data)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place');
     }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
-request.send()
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+request.send() 
