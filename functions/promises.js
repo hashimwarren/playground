6af45bae54d8f1592513595bdd0669918ebb6bcd
Hashim Warren <hashimwarren@gmail.com>
2020-11-08 23:12:04 -0500

    Halfway though challenge

--- functions/promises.js
+++ functions/promises.js
@@ -1,38 +1,65 @@
-const getDataCallback = (callback) => {
+const getDataCallback = (num, callback) => {
     setTimeout(() => {
-        callback('this is my callback error', undefined)
+        if (typeof num === 'number') {
+            callback(undefined, num * 2)
+        } else {
+            callback('Number must be provided')
+        }
+
 
     }, 2000);
 
 }
 
 
-getDataCallback((err, data) => {
+getDataCallback(2, (err, data) => {
     if (err) {
         console.log(err);
 
     } else {
-        console.log(data);
+        getDataCallback(data, (err, data) => {
+            if (err) {
+                console.log('err');
+
+            } else {
+                console.log(data);
+
+            }
+        })
 
     }
 })
 
 // Promise
 
-const getDataPromise = (data) => new Promise((resolve, reject) => {
+const getDataPromise = (num) => new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(`this is my success data: ${data}`)
-        //reject('this is my promise error')
+        typeof num === 'number' ? resolve(num * 2) : reject('Number must be provided')
 
     }, 2000)
 })
 
 
-const myPromise = getDataPromise(123)
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log(`Promise data: ${data}`);
+    }, (err) => {
+        console.log(err);
 
-myPromise.then((data) => {
-    console.log(data);
+    })
+
 }, (err) => {
+    console.log(err);
+
+})
+
+getDataPromise(10).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    console.log(data)
+}).catch((err) => {
     console.log(err);
 })
 
