30ebd8745f4497596888f2be06cc917bd8f88629
Hashim Warren <hashimwarren@gmail.com>
2021-04-03 20:28:16 -0400

    Set up webpack

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -1,9 +1,10 @@
-const path = required('path')
+const path = require('path')
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'public/scripts')
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: 'bundle.js'
     }
 }
 
