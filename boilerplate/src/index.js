8cd888a38dd32b8a1e135ae3466f81415bcfeeb0
Hashim Warren <hashimwarren@gmail.com>
2021-04-17 12:09:17 -0400

    Add webpack server

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -4,7 +4,7 @@ import otherSquare, { add, name } from './utilities'
 import scream from "./scream";
 
 console.log('index.js');
-console.log(add(31, 1));
+console.log(add(31, 5));
 console.log(name);
 console.log(scream(name));
 console.log(otherSquare(12));
--- boilerplate/src/utilities.js
+++ boilerplate/src/utilities.js
@@ -1,11 +1,12 @@
 // named export
 // default export
 
+//TODO fix webpack servers
 
 const add = (a, b) => a + b
 
-const name = 'Hashim Warren'
+const name = 'Madison Georgiann Warren'
 
 const square = (x) => x * x
-
+console.log('from my code');
 export { add, name, square as default }
